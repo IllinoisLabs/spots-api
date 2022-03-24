521f29cc5dd7b225bc4b47676e0f2a198795c243
mjneff2 <mjneff922@gmail.com>
2022-03-23 20:38:31 -0500

    Cleaned up json encoding to use bson to avoid any future issues

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import json
 import urllib
 import os
-from dataclasses import dataclass
-from bson import ObjectId
+from bson import json_util
 
 from flask import Flask, Response, jsonify
 
-from pymongo import MongoClient
-from pymodm.connection import connect
+from pymodm.connection import connect, _get_connection
 from dotenv import load_dotenv
 
 from utils.models import Spot, Review
@@ -15,50 +13,26 @@ from utils.models import Spot, Review
 load_dotenv()
 CONN_STR = f"mongodb+srv://illinoislabs:{urllib.parse.quote_plus(os.environ['MONGO_PASSWORD'])}@spotsdb.vayoj.mongodb.net/{urllib.parse.quote_plus(os.environ['MONGO_DB'])}?retryWrites=true&w=majority"
 connect(CONN_STR)
-conn = MongoClient(CONN_STR)
 app = Flask(__name__)
-
-
-@dataclass
-class FlaskResponse:
-    """
-    Represents a Flask response.
-    to_resp() a payload and a status code.
-    """
-    payload: Response
-    code: int
-
-    def to_resp(self):
-        return self.payload, self.code
-
-
-class JSONEncoder(json.JSONEncoder):
-    """
-    We create our own JSONEncoder to handle MongoDB IDs of type ObjectId
-    when serializing objects.
-    """
-    def default(self, o):
-        if isinstance(o, ObjectId):
-            return str(o)
-        return json.JSONEncoder.default(self, o)
-
 
 @app.route("/")
 def index():
+    conn = _get_connection()
     payload = {
-        k: f"/{name.lower()}" for k, name in enumerate(conn["spots"].list_collection_names())
+        k: f"/{name.lower()}" for k, name in enumerate(conn.database.list_collection_names())
     }
-    return FlaskResponse(jsonify({"documents": payload}), 200).to_resp()
+    return jsonify({"documents": payload}), 200
 
 
 @app.route("/spots")
 def spots():
     # Get list of all spots in the database
-    encoder = JSONEncoder()
-    spots = [encoder.encode(x) for x in Spot.objects.all().values()]
-    return FlaskResponse(
-        jsonify(spots), 200
-    ).to_resp()
+    spots = [x for x in Spot.objects.all().values()]
+    return Response(
+        response=json_util.dumps(spots),
+        status=200,
+        mimetype="application/json"
+    )
 
 
 if __name__ == "__main__":
